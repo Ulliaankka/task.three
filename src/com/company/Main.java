61628f2e72bc9c7ef7e2cb160fa35189f6199160
Ulianka <ulliaankka@gmail.com>
2020-03-31 12:20:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args) {
+	// Заповніть Array.list і виведіть його значення на консоль
+        ArrayList <String> cities = new ArrayList<>();
+        cities.add("Berlin") ;
+        cities.add("New York");
+        cities.add("Paris");
+
+        System.out.println(cities);
+    }
+}
